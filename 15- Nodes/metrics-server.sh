d9db5a59d90c31d92f6dc05ad4be1fbf550eac03
Ahmed ElBakry <aelbakry.wayfair.com>
2022-02-22 15:03:52 +0100

    Adding the working yamlfiles

---
+++ 15- Nodes/metrics-server.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl patch deployment metrics-server -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"metrics-server","args":["--cert-dir=/tmp", "--secure-port=4443", "--kubelet-insecure-tls","--kubelet-preferred-address-types=InternalIP"]}]}}}}'
